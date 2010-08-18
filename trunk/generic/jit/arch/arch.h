275a7dbc73ffd95e344e7d1c6bfa3d93deb69895
Guilherme Polo <gpolo@bitbucket.com>
2010-08-17 23:21:31 -0300

    Adjusting the ifdef so Tcl compiles when TCL_JIT is not defined.

--- trunk/generic/jit/arch/arch.h
+++ trunk/generic/jit/arch/arch.h
@@ -1,10 +1,10 @@
 #ifndef TCLJIT_ARCH_H
 #define TCLJIT_ARCH_H
 
-#ifdef TCL_JIT
-    #ifdef __i386__
-        #include "x86/mcode.h"
-    #else
+#ifdef __i386__
+    #include "x86/mcode.h"
+#else
+    #ifdef TCL_JIT
         /* XXX Instead of stopping the compilation with an error we
          * could simply not use the JIT system. */
         #error "Compiler and/or architecture not supported."
