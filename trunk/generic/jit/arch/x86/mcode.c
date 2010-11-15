1678b0f7bcf39b33b743adab0a0d796171978108
Guilherme Polo <gpolo@bitbucket.com>
2010-11-14 23:15:41 -0200

    A lot of new code for x86 code generation.

--- trunk/generic/jit/arch/x86/mcode.c
+++ trunk/generic/jit/arch/x86/mcode.c
@@ -11,9 +11,13 @@
 
 static Tcl_HashTable labels, toPatch;
 static int labelsTableInitialized = 0;
+static int generatedLabelCount;
 
 struct needPatching {
-    unsigned char *addr;
+    //unsigned char *addr;
+    //unsigned char *addr;
+    ptrdiff_t *addr;
+
     int size, used;
 };
 
@@ -35,15 +39,10 @@ finishLabelTable(void)
         Tcl_DeleteHashTable(&toPatch);
         Tcl_DeleteHashTable(&labels);
         labelsTableInitialized = 0;
+        generatedLabelCount = 0;
     }
 }
 
-int
-allocReg(void *elem)
-{
-    /* XXX */
-    return EAX;
-}
 
 static ptrdiff_t
 getRelative(struct MCode *code, const char *label)
@@ -60,14 +59,17 @@ getRelative(struct MCode *code, const char *label)
         he = Tcl_FindHashEntry(&toPatch, label);
         if (he == NULL) {
             patch = malloc(sizeof(struct needPatching));
-            patch->size = 4;
-            patch->addr = malloc(patch->size * sizeof(char));
+            /* XXX Using a somewhat large size because the Tcl hash table
+             * seems to have problems with realloc. */
+            patch->size = 32;
+            //patch->addr = malloc(patch->size * sizeof(char));
+            patch->addr = malloc(patch->size * sizeof(ptrdiff_t));
             patch->used = 0;
             he = Tcl_CreateHashEntry(&toPatch, label, &notused);
             Tcl_SetHashValue(he, patch);
         }
         patch = Tcl_GetHashValue(he);
-        ADJUST_SIZE_PATCHLIST(patch);
+        //ADJUST_SIZE_PATCHLIST(patch); /* See XXX comment above. */
         patch->addr[patch->used++] = ((code->codeEnd) - (code->mcode));
         result = 0x00;
     } else {
@@ -77,26 +79,69 @@ getRelative(struct MCode *code, const char *label)
     return result;
 }
 
+/* je rel32 */
 void
 jumpEqual(struct MCode *code, const char *label)
 {
-    *(code->codeEnd)++ = 0x74;
+#if 0
+    *(code->codeEnd)++ = 0x74; /* jz/je rel8 */
     *(code->codeEnd)++ = getRelative(code, label);
+#endif
+    int target;
+
+    *(code->codeEnd)++ = 0x0F;
+    *(code->codeEnd)++ = 0x84;
+    target = getRelative(code, label);
+    IMM32(code->codeEnd, target);
 }
 
+/* jne rel32 */
 void
 jumpNotEqual(struct MCode *code, const char *label)
 {
-    *(code->codeEnd)++ = 0x75;
+#if 0
+    *(code->codeEnd)++ = 0x75; /* jnz/jne rel8 */
     *(code->codeEnd)++ = getRelative(code, label);
+#endif
+    int target;
+
+    *(code->codeEnd)++ = 0x0F;
+    *(code->codeEnd)++ = 0x85;
+    target = getRelative(code, label);
+    if (target != 0) {
+        //ptrdiff_t addr = code->codeEnd - code->mcode;
+        //printf("******* %s %d %d\n", label, target,
+        //        -((code->codeEnd - code->mcode - 2) - target));
+        /* Jump backwards. */
+        DEBUG("*** Backwards jump.\n");
+        target = -((code->codeEnd - code->mcode - 2) - target) - 6;
+    }
+    IMM32(code->codeEnd, target);
 }
 
+/* jmp rel32 */
 void
 jumpTo(struct MCode *code, const char *label)
 {
-
-    *(code->codeEnd)++ = 0xEB;
+#if 0
+    *(code->codeEnd)++ = 0xEB; /* jmp rel8 */
     *(code->codeEnd)++ = getRelative(code, label);
+#endif
+    int target;
+
+    *(code->codeEnd)++ = 0xE9;
+    target = getRelative(code, label);
+    IMM32(code->codeEnd, target);
+}
+
+char *
+uniqueLabel(void)
+{
+    char *label = malloc(22 * sizeof(char));
+    generatedLabelCount++;
+
+    snprintf(label, 22, "L%d", generatedLabelCount);
+    return label;
 }
 
 void
@@ -119,11 +164,148 @@ makeLabel(struct MCode *code, const char *label)
         struct needPatching *patch = Tcl_GetHashValue(he);
 
         for (i = 0; i < patch->used; i++) {
-            *(code->mcode + patch->addr[i]) = (addr - patch->addr[i] - 1);
+            DEBUG("PATCH AT %d, %d\n",
+                    patch->addr[i],
+                    addr - patch->addr[i] - 4);
+            //*(code->mcode + patch->addr[i]) = (addr - patch->addr[i] - 1);
+            *(code->mcode + patch->addr[i]) = (addr - patch->addr[i] - 4);
+            *(code->mcode + patch->addr[i] + 1) = (addr - patch->addr[i] - 4) >> 8;
+            *(code->mcode + patch->addr[i] + 2) = (addr - patch->addr[i] - 4) >> 16;
+            *(code->mcode + patch->addr[i] + 3) = (addr - patch->addr[i] - 4) >> 24;
         }
 
         free(patch->addr);
         free(patch);
         Tcl_DeleteHashEntry(he);
     }
+}
+
+
+/* Load local variable into EAX. */
+inline void
+//load_localvar(struct MCode *code, struct Quadruple *ptr)
+load_localvar(struct MCode *code, struct Quadruple *ptr, int stindex)
+{
+    int reg;
+    long int offset;
+
+    reg = EAX;
+    /* Copy the first param (Interp) passed to the JIT func to reg. */
+    COPY_PARAM_REG(code->codeEnd, 0, reg);
+
+    /* reg is now pointing at an Interp struct. */
+
+    /* Point to the CallFrame struct. */
+    offset = offsetof(Interp, varFramePtr);
+    MOV_DISP8DREG_REG(code->codeEnd, offset, reg, reg);
+
+    /* Point to the Var structs. */
+    offset = offsetof(CallFrame, compiledLocals);
+    MOV_DISP8DREG_REG(code->codeEnd, offset, reg, reg);
+
+    if (ptr->src_a->offset) {
+        /* Point to the correct element in the Var array. */
+        ADD_IMM8_REG(code->codeEnd, sizeof(Var) * ptr->src_a->offset, reg);
+    }
+
+    /* Point to the expected Tcl_Obj. */
+    offset = offsetof(Var, value.objPtr);
+    MOV_DISP8DREG_REG(code->codeEnd, offset, reg, reg);
+
+    /* XXX Testing: get the integer value */
+    AND_IMM8_REG(code->codeEnd, -16, ESP); // XXX Alignment!
+    PUSH_REG(code->codeEnd, EBX);
+
+    /* Get integer from obj. */
+#if 0
+    COPY_PARAM_REG(code->codeEnd, 0, ECX);
+    PUSH_REG(code->codeEnd, ECX); /* (Interp *) */
+    PUSH_REG(code->codeEnd, reg); /* (Tcl_Obj *) */
+    PUSH_REG(code->codeEnd, EDX); /* Where to store the integer value. */
+#endif
+    LEA_DISP8DREG_REG(code->codeEnd, stindex, EBP, EDX);
+    //LEA_DISP32DREG_REG(code->codeEnd, stindex, EBP, EDX);
+
+    PUSH_REG(code->codeEnd, EDX);          /* Where to store the integer value. */
+    PUSH_REG(code->codeEnd, reg);          /* (Tcl_Obj *) */
+    PUSH_DISP8DREG(code->codeEnd, 8, EBP); /* (Interp *) */
+
+
+    MOV_IMM32_REG(code->codeEnd, (ptrdiff_t)Tcl_GetLongFromObj, EBX);
+    CALL_ABSOLUTE_REG(code->codeEnd, EBX);
+
+    ADD_IMM8_REG(code->codeEnd, 12, ESP);
+
+    POP_REG(code->codeEnd, EBX);
+
+    /* Checking return from Tcl_GetIntFromObj. */
+    CMP_IMM8_REG(code->codeEnd, 0, EAX);
+    JUMP_NOTEQ(code, "LINTERP");
+
+    /* Save the value. */ /* XXX Isn't it saved already ? */
+//    MOV_DREG_REG(code->codeEnd, EDX, EDX);
+//    MOV_REG_DISP8DREG(code->codeEnd, EDX, stindex, EBP);
+    /* XXX End testing. */
+
+    //return reg;
+}
+
+/* Load Tcl object at stack pos. */
+inline void
+load_tclobject(struct MCode *code, int obindex, int stindex)
+{
+    int reg;
+    long int offset;
+
+    reg = EAX;
+    /* Copy the second param (ByteCode) passed to the JIT func to reg. */
+    COPY_PARAM_REG(code->codeEnd, 1, reg);
+
+    /* reg is now pointing at an ByteCode struct. */
+
+    /* Point to the Tcl_Obj array of literal objects. */
+    offset = offsetof(ByteCode, objArrayPtr);
+    MOV_DISP8DREG_REG(code->codeEnd, offset, reg, reg);
+
+    if (obindex) {
+        /* Point to the correct object in this array. */
+        ADD_IMM8_REG(code->codeEnd, sizeof(Tcl_Obj *) * obindex, reg);
+    }
+
+    /* Get the Tcl_Obj address. */
+    MOV_DREG_REG(code->codeEnd, reg, reg);
+
+    /* Save reg to the expected stack position. */
+//    MOV_REG_DISP8DREG(code->codeEnd, reg, stindex, EBP);
+//    return;
+
+
+    /* XXX Testing (Need to check for empty string before continuing!) */
+    /* Convert to an integer. */
+    AND_IMM8_REG(code->codeEnd, -16, ESP); // XXX Alignment!
+    PUSH_REG(code->codeEnd, EBX);
+
+    //PUSH_REG(code->codeEnd, EDX); /* Where to store the integer value. */
+    //printf(">>> %d\n", stindex);
+    LEA_DISP8DREG_REG(code->codeEnd, stindex, EBP, EDX);
+    //LEA_DISP32DREG_REG(code->codeEnd, stindex, EBP, EDX);
+
+    PUSH_REG(code->codeEnd, EDX);
+    PUSH_REG(code->codeEnd, reg); /* (Tcl_Obj *) */
+    PUSH_DISP8DREG(code->codeEnd, 8, EBP);
+
+    MOV_IMM32_REG(code->codeEnd, (ptrdiff_t)Tcl_GetLongFromObj, EBX);
+    CALL_ABSOLUTE_REG(code->codeEnd, EBX);
+
+    ADD_IMM8_REG(code->codeEnd, 12, ESP);
+
+    POP_REG(code->codeEnd, EBX);
+
+    /* Checking return from Tcl_GetLongFromObj. */
+    CMP_IMM8_REG(code->codeEnd, 0, EAX);
+    JUMP_NOTEQ(code, "LINTERP");
+
+    /* Save the value. */
+//    MOV_DREG_REG(code->codeEnd, EDX, EDX);
+//    MOV_REG_DISP8DREG(code->codeEnd, EDX, stindex, EBP);
 }
--- trunk/generic/jit/arch/x86/mcode.h
+++ trunk/generic/jit/arch/x86/mcode.h
@@ -5,21 +5,39 @@
 
 typedef enum { EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI } regs;
 
-int allocReg(void *);
 void initLabelTable(void);
 void finishLabelTable(void);
 void jumpEqual(struct MCode *, const char *);
 void jumpNotEqual(struct MCode *, const char *);
 void jumpTo(struct MCode *, const char *);
+char *uniqueLabel(void);
 void makeLabel(struct MCode *, const char *);
+//inline int load_localvar(struct MCode *, struct Quadruple *);
+inline void load_localvar(struct MCode *, struct Quadruple *, int);
+inline void load_tclobject(struct MCode *, int, int);
 
 /* Auxiliary macros. Most of these may need to be ported to
  * different instruction sets. */
+#define INDEX_TO_STACKADDR(index) (-((index + 1) * 4))
+
 #define IMM32(code, v) \
     *code++ = v; *code++ = v >> 8; *code++ = v >> 16; *code++ = v >> 24
 
-#define PROLOGUE(code) PUSH_REG(code, EBP); MOV_REG_REG(code, ESP, EBP)
-#define EPILOGUE(code) LEAVE(code); RETN(code)
+#define PROLOGUE(code, stsize) { \
+    PUSH_REG(code, EBP); MOV_REG_REG(code, ESP, EBP); \
+    if (4 * stsize > 0) { \
+        if (4 * stsize > 127) { SUB_IMM32_REG(code, 4 * stsize, ESP); } \
+        else { SUB_IMM8_REG(code, 4 * stsize, ESP); } \
+    } \
+}
+
+#define EPILOGUE(code, stsize) { \
+    if (4 * stsize > 0) { \
+        if (4 * stsize > 127) { ADD_IMM32_REG(code, 4 * stsize, ESP); } \
+        else { ADD_IMM8_REG(code, 4 * stsize, ESP); } \
+    } \
+    LEAVE(code); RETN(code); \
+}
 
 #define LABEL(mcode, label) makeLabel(mcode, label)
 
@@ -45,11 +63,16 @@ void makeLabel(struct MCode *, const char *);
     *code++ = 0xFF; \
     *code++ = MODRM(0x0, 1, reg)
 
-#define PUSH_DISP8REG(code, disp, reg) \
+#define PUSH_DISP8DREG(code, disp, reg) \
     *code++ = 0xFF; \
     *code++ = MODRM(0x1, 6, reg); \
     *code++ = disp
+#define PUSH_DISP32DREG(code, disp, reg) \
+    *code++ = 0xFF; \
+    *code++ = MODRM(0x2, 6, reg); \
+    IMM32(code, disp)
 #define PUSH_REG(code, reg) *code++ = 0x50 + reg
+#define PUSH_DREG(code, reg) *code++ = 0xFF; *code++ = MODRM(0, 6, reg)
 #define POP_REG(code, reg) *code++ = 0x58 + reg
 
 /* XXX Se tirar o parenteses mais externo em ((unsighed char*) ... )
@@ -63,35 +86,152 @@ void makeLabel(struct MCode *, const char *);
     *code++ = 0xFF; \
     *code++ = MODRM(0x3, 2, reg)
 
+/* cmpl %src, %dest */
 #define CMP_REG_REG(code, src, dest) \
     *code++ = 0x39; \
     *code++ = MODRM(0x3, src, dest)
+/* cmpl %src, %(dest) */
+#define CMP_REG_DREG(code, src, dest) \
+    *code++ = 0x39; \
+    *code++ = MODRM(0, src, dest)
+/* cmpl $imm32, %reg */
+#define CMP_IMM32_REG(code, imm, reg) \
+    *code++ = 0x81; \
+    *code++ = MODRM(0x3, 7, reg); \
+    IMM32(code, imm)
+#define CMP_IMM8_REG(code, imm, reg) \
+    *code++ = 0x83; \
+    *code++ = MODRM(0x3, 7, reg); \
+    *code++ = imm
 
-#define XOR_REG_REG(code, src, dest) \
-    *code++ = 0x33; \
+/* imul %src, %dest */ /* XXX Check MODRM order of src, dest */
+#define MUL_REG_REG(code, src, dest) \
+    *code++ = 0x0F; \
+    *code++ = 0xAF; \
     *code++ = MODRM(0x3, src, dest)
+/* imul %src, $imm, %dest */
+#define MUL_IMM32_REG_REG(code, imm32, src, dest) \
+    *code++ = 0x69; \
+    *code++ = MODRM(0x3, dest, src); \
+    IMM32(code, imm32)
+/* imul %reg */
+#define MUL_REG(code, reg) \
+    *code++ = 0xF7; \
+    *code++ = MODRM(0x3, 5, reg)
+/* imul (%reg) */
+#define MUL_DREG(code, reg) \
+    *code++ = 0xF7; \
+    *code++ = MODRM(0, 5, reg)
+
+/* idiv %reg */
+#define DIV_REG(code, reg) \
+    *code++ = 0xF7; \
+    *code++ = MODRM(0x3, 7, reg)
+
+/* XXX Fixed a bug here, was using 0x33 instead (and saving
+ * the result in the source register instead of dest reg).
+ * Note: Always be twice careful with machine code. */
+#define XOR_REG_REG(code, src, dest) \
+    *code++ = 0x31; \
+    *code++ = MODRM(0x3, src, dest)
+#define XOR_REG_DREG(code, src, dest) \
+    *code++ = 0x31; \
+    *code++ = MODRM(0, src, dest)
 
 #define AND_IMM8_REG(code, imm, reg) \
     *code++ = 0x83; \
     *code++ = MODRM(0x3, 4, reg); \
     *code++ = imm
 
+/* addl (%src), %dest */
+#define ADD_DREG_REG(code, src, dest) \
+    *code++ = 0x03; \
+    *code++ = MODRM(0, src, dest)
+
+/* addl %src, (%dest) */
+#define ADD_REG_DREG(code, src, dest) \
+    *code++ = 0x01; \
+    *code++ = MODRM(0, src, dest)
+
+/* addl %src, %dest */
+#define ADD_REG_REG(code, src, dest) \
+    *code++ = 0x01; \
+    *code++ = MODRM(0x3, src, dest)
+
+/* addl $imm8, %reg */
 #define ADD_IMM8_REG(code, imm, reg) \
     *code++ = 0x83; \
     *code++ = MODRM(0x3, 0, reg); \
     *code++ = imm
 
+#define ADD_IMM32_REG(code, imm, reg) \
+    *code++ = 0x81; \
+    *code++ = MODRM(0x3, 0, reg); \
+    IMM32(code, imm)
+
+/* leal imm8(%src), %dest */
+#define LEA_DISP8DREG_REG(code, disp, src, dest) \
+    *code++ = 0x8d; \
+    *code++ = MODRM(0x1, dest, src); \
+    *code++ = disp
+/* leal imm32(%src), %dest */
+#define LEA_DISP32DREG_REG(code, disp, src, dest) \
+    *code++ = 0x8d; \
+    *code++ = MODRM(0x2, dest, src); \
+    IMM32(code, disp)
+/* leal (%src), %dest */
+#define LEA_DREG_REG(code, src, dest) \
+    *code++ = 0x8d; \
+    *code++ = MODRM(0, dest, src)
+
+#define SUB_IMM8_REG(code, imm, reg) \
+    *code++ = 0x83; \
+    *code++ = MODRM(0x3, 0x5, reg); \
+    *code++ = imm
+#define SUB_IMM32_REG(code, imm, reg) \
+    *code++ = 0x81; \
+    *code++ = MODRM(0x3, 0x5, reg); \
+    IMM32(code, imm)
+#define SUB_REG_DREG(code, src, dest) \
+    *code++ = 0x29; \
+    *code++ = MODRM(0, src, dest)
+#define SUB_REG_REG(code, src, dest) \
+    *code++ = 0x29; \
+    *code++ = MODRM(0x3, src, dest)
+
 #define JUMP_EQ(mcode, label) jumpEqual(mcode, label)
 #define JUMP_NOTEQ(mcode, label) jumpNotEqual(mcode, label)
 #define GOTO(mcode, label) jumpTo(mcode, label)
 
-#define SET_NE_REG(code, reg) \
+/* reg_a AND reg_b, set flags */
+#define TEST_REG_REG(code, reg_a, reg_b) \
+    *code++ = 0x85; \
+    *code++ = MODRM(0x3, reg_a, reg_b)
+
+/* SETcc opcodes */
+#define SET_NEQ_REG(code, reg) \
     *code++ = 0x0F; \
     *code++ = 0x95; \
     *code++ = MODRM(0x3, 0, reg)
 #define SET_EQ_REG(code, reg) \
     *code++ = 0x0F; \
     *code++ = 0x94; \
+    *code++ = MODRM(0x3, 0, reg)
+#define SET_LT_REG(code, reg) \
+    *code++ = 0x0F; \
+    *code++ = 0x9C; \
+    *code++ = MODRM(0x3, 0, reg)
+#define SET_LE_REG(code, reg) \
+    *code++ = 0x0F; \
+    *code++ = 0x9E; \
+    *code++ = MODRM(0x3, 0, reg)
+#define SET_GT_REG(code, reg) \
+    *code++ = 0x0F; \
+    *code++ = 0x9F; \
+    *code++ = MODRM(0x3, 0, reg)
+#define SET_GE_REG(code, reg) \
+    *code++ = 0x0F; \
+    *code++ = 0x9D; \
     *code++ = MODRM(0x3, 0, reg)
 
 /* movl %reg, %reg */
@@ -107,10 +247,41 @@ void makeLabel(struct MCode *, const char *);
     *code++ = 0x8B; \
     *code++ = MODRM(0x1, dest, src); \
     *code++ = disp
+/* movl disp32(%reg), %reg */
+#define MOV_DISP32DREG_REG(code, disp, src, dest) \
+    *code++ = 0x8B; \
+    *code++ = MODRM(0x2, dest, src); \
+    IMM32(code, disp)
+/* movl %reg, disp8(%reg) */
+#define MOV_REG_DISP8DREG(code, src, disp, dest) \
+    *code++ = 0x89; \
+    *code++ = MODRM(0x1, src, dest); \
+    *code++ = disp
 /* movl (%reg), %reg */
 #define MOV_DREG_REG(code, src, dest) \
     *code++ = 0x8B; \
     *code++ = MODRM(0x0, dest, src)
+/* movl %reg, (%reg) */
+/* XXX :) There was a bug here because I used
+ * MODRM(0, dest, src) instead of MODRM(0, src, dest) */
+#define MOV_REG_DREG(code, src, dest) \
+    *code++ = 0x89; \
+    *code++ = MODRM(0, src, dest)
+
+#define CMOVLT_REG_REG(code, src, dest) \
+    *code++ = 0x0F; \
+    *code++ = 0x4C; \
+    *code++ = MODRM(0x3, src, dest)
+
+
+/* shr %cl, %reg */
+#define SHR_CL_REG(code, reg) \
+    *code++ = 0xD3; \
+    *code++ = MODRM(0x3, 5, reg)
+/* shr %cl, %(reg) */
+#define SHR_CL_DREG(code, reg) \
+    *code++ = 0xD3; \
+    *code++ = MODRM(0, 5, reg)
 
 #define LEAVE(code) *code++ = 0xC9
 #define RETN(code) *code++ = 0xC3 /* Near return. */
