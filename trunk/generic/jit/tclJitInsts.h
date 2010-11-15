b4bc3ceae09d860b59a2bb2079c541e16e77db35
Guilherme Polo <gpolo@bitbucket.com>
2010-11-14 23:12:12 -0200

    New JIT instructions.

--- trunk/generic/jit/tclJitInsts.h
+++ trunk/generic/jit/tclJitInsts.h
@@ -1,12 +1,7 @@
-/*
- * Created:  13/07/2010 15:49:20
- *
- * Author:  Guilherme Polo, ggpolo@gmail.com
- *
- */
 #ifndef TCLJIT_INSTS_H
 #define TCLJIT_INSTS_H
 
+#define JIT_INST_SAVE 0
 #define JIT_INST_MOVE 1
 #define JIT_INST_CALL 6
 #define JIT_INST_GOTO 34
@@ -14,6 +9,7 @@
 #define JIT_INST_JFALSE 38
 #define JIT_INST_ADD 53
 #define JIT_INST_INCR 29
+#define JIT_INST_NOP (LAST_INST_OPCODE + 1)
 
 #endif /* TCLJIT_INSTS_H */
 
